4325f56268e3aa24fffa25db17cacb998f6b01d8
mmonteirocl-torre <miguelmonteiroclaveri@gmail.com>
2020-05-08 18:38:54 +0200

    Estamos en proceso de seguir configurando nuestra carta en flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,55 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+/*
+* Si es un stateless widget
+* podemos usar perfectamente el hotreload
+* */
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/me.jpeg'),
+              ),
+              SizedBox(
+                height: 15,
+                width: double.infinity,
+              ),
+              Text(
+                "Miguel Monteiro",
+                style: TextStyle(
+                    fontSize: 35,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Prime'),
+              ),
+              Text(
+                "FULL-STACK DEV",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.teal.shade50,
+                  fontFamily: 'Source Sans Pro',
+                  letterSpacing: 2.5,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
